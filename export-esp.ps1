0d808b4cfa6ac1192df369be9c2c608913e5263f
Seb Ospina <kraige@gmail.com>
2024-03-11 22:26:16 +0100

    esp-wifi for ble/wifi/esp-now
    Adding dependency for ble and esp-now

---
+++ export-esp.ps1
@@ -0,0 +1,3 @@
+$Env:LIBCLANG_PATH = "C:\Users\sebos\.rustup\toolchains\esp\xtensa-esp32-elf-clang\esp-clang\bin\libclang.dll"
+$Env:PATH = "C:\Users\sebos\.rustup\toolchains\esp\xtensa-esp32-elf-clang\esp-clang\bin;" + $Env:PATH
+$Env:PATH = "C:\Users\sebos\.rustup\toolchains\esp\xtensa-esp-elf\bin;" + $Env:PATH
